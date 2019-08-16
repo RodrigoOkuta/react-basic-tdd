0484e0432d7670737f8d81f9cd6e67fde3f74609
Rodrigo Okuta <rodrigo.okuta@outlook.com>
2019-08-16 10:10:22 -0400

    basic TDD for react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleIncrementClick = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  };
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The currently counter is: {counter}</h1>
+        <button
+          data-test="increment-button"
+          onClick={this.handleIncrementClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Increment Counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
